e45147450ea20d73299269b926ba2ed982aa0a71
Kasula Sricharan <kasulasricharan0508@gmail.com>
2024-05-30 10:48:44 +0530

    first commit

---
+++ mail.py
@@ -0,0 +1,34 @@
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from email.mime.text import MIMEText
+from base64 import urlsafe_b64encode
+import os
+#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
+# OAuth 2.0 setup
+SCOPES = ['https://www.googleapis.com/auth/gmail.send']
+creds = None
+if os.path.exists('token.json'):
+    creds = flow.credentials
+else:
+    flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+    creds = flow.run_local_server(port=0)
+
+# Gmail API setup
+service = build('gmail', 'v1', credentials=creds)
+
+# List of recipient email addresses
+recipient_emails = []#add recipients' addressess
+a=0
+# Create a MIMEText object for the email content
+for recipient_email in recipient_emails:
+    message = MIMEText("Dear Sir/Ma'am,\nGreetings From Veltron Equipments.\nWe thank you very much for your kind visit to our stall at the recently concluded HIPLEX - International Plastics Show held in Hyderabad.\nWe appreciate your kind gesture in showing interest in our products.\n\nWe are manufacturers of below mentioned products:\n\tCORONA TREATERS\n\tAUTO LOADERS\n\tHOPPER DRIERS\n\tDEHUMIDIFIERS\n\tINDUCTION CAP SEALERS\n\nWe look forward to your revert and assist you on above products.\n\nBest regards,\nVenkatesh\n9394704575\n9533704575")
+    message['to'] =  recipient_email # Join the recipient emails with commas
+    message['subject'] = 'HI-PLEX 2023'
+
+# Send the email
+    raw_message = urlsafe_b64encode(message.as_bytes()).decode('utf-8')
+    message = {'raw': raw_message}
+    service.users().messages().send(userId='me', body=message).execute()
+    print('Email sent successfully!')
+    a=a+1
+    print(a)
